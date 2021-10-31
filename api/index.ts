45c7b56ef0c590fe08ab93a087c2df4aa9f30c36
whitedevil31 <magz3116@gmail.com>
2021-10-31 14:27:33 +0530

    Use new endpoints

--- api/index.ts
+++ api/index.ts
@@ -6,9 +6,9 @@ import { HttpError } from "http-errors";
 dotenv.config({ path: "./.env" });
 connectDB();
 
-// import authRoute from "./auth/auth.router";
-// import adminRoute from "./admin/admin.router";
-// import clientRoute from "./client/client.router";
+import authRoute from "./auth/auth.router";
+import adminRoute from "./admin/admin.router";
+import clientRoute from "./client/client.router";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -16,21 +16,21 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-// app.use("/", (req, res) => {
-//   res.json({ success: true, message: "Namma lost maara !" });
-// });
-// app.use(authRoute);
-// app.use(adminRoute);
-// app.use(clientRoute);
+app.use("/", (req, res) => {
+  res.json({ success: true, message: "Namma lost maara !" });
+});
+app.use(authRoute);
+app.use(adminRoute);
+app.use(clientRoute);
 
 app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
-    if (error) {
-        res.status(error.status).json({ message: error.message });
-    }
+  if (error) {
+    res.status(error.status).json({ message: error.message });
+  }
 });
-// app.use("*", (req: Request, res: Response) => {
-//   res.status(404).json({ message: "Resource not found" });
-// });
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).json({ message: "Resource not found" });
+});
 app.listen(port, () => {
-    console.log(`🎉🎉 - Wakey Wakey, your Server is up and running at ${port}!`);
+  console.log(`🎉🎉 - Wakey Wakey, your Server is up and running at ${port}!`);
 });
